fdf5bae7247823692c85abaa7858524afdbc915b
webapp23929 <ibaraki23nm024f@gmail.com>
2023-10-08 22:32:42 +0900

    ブログ投稿作成画面作成

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,6 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PostController;
 Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
