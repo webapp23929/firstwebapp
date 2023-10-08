08f50b330ef2a4a8ac14fa3c2eaae70e99ba9c27
webapp23929 <ibaraki23nm024f@gmail.com>
2023-10-08 19:12:33 +0900

    カリキュラム08-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,4 +14,16 @@ class PostController extends Controller
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
         //getPaginateByLimit()はPost.phpで定義したメソッドです。
     }
+    /**
+    * 特定IDのpostを表示する
+    *
+    * @params Object Post // 引数の$postはid=1のPostインスタンス
+    * @return Reposnse post view
+    */
+    public function show(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+    {
+        //dd($post);
+        return view('posts.show')->with(['post' => $post]);
+        //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
 }
