1c1def02b237790da24acb47721abc85006ea172
webapp23929 <ibaraki23nm024f@gmail.com>
2023-10-10 01:07:42 +0900

    リレーション、カテゴリ

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\PostRequest;
 use App\Models\Post;
+use App\Models\Category;
 
 class PostController extends Controller
 {
@@ -28,10 +29,12 @@ class PostController extends Controller
         //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
     }
     
+    /**
     public function create()
     {
         return view('posts/create');
     }
+    */
     
     public function store(PostRequest $request, Post $post)
     {
@@ -57,5 +60,11 @@ class PostController extends Controller
     {
         $post->delete();
         return redirect('/');
+    }
+    
+
+    public function create(Category $category)
+    {
+        return view('posts.create')->with(['categories' => $category->get()]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;   //追加
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,4 @@ Route::post('/posts', [PostController::class, 'store']);
 Route::get('posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('posts/{post}', [PostController::class, 'update']);
 Route::delete('/posts/{post}', [PostController::class, 'delete']);
+Route::get('/categories/{category}', [CategoryController::class,'index']);
